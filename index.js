08c6975e3cd7007c8cbecf18b65ff495d3ba04b8
PushkarDureja <pushkar.dureja@pepcoding.com>
2021-09-26 17:07:49 +0530

    Updates

--- index.js
+++ index.js
@@ -1,44 +1,32 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+require("dotenv/config");
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+const Redis = require("ioredis");
+const sub = new Redis();
+const pub = new Redis();
+sub.subscribe("test"); // From now, `sub` enters the subscriber mode.
 
-const io = require("socket.io")({
-    cors: {
-        origin: "*",
-    },
+sub.on("message", (channel, message) => {
+  console.log("new message: ", message);
+  io.sockets.in("room").emit("new-message", message);
 });
 
-const ior = require('ioredis')
-const REDIS_ADDRESS = '127.0.0.1:6379'
+io.on("connection", (socket) => {
+  // ...
+  console.log(`socket client ${socket.id} connected to server`);
+  socket.join("room");
+  socket.on("send", (msg) => {
+    pub.publish("test", `New message from ${socket.id} : ${msg}`);
+    // socket.emit("new-message", msg);
+  });
+});
 
-// testing call
-// redis.ping().then(console.log).catch(console.error)
-
-
-// redis.setex("test", 600,"this is test message").then(console.log).catch(console.log)
-
-io.on("connection", function(socket){
-    const subscriber = new ior(REDIS_ADDRESS)
-    const publisher = new ior(REDIS_ADDRESS)
-    
-    socket.join("messages");
-
-    subscriber.subscribe(`messages`)
-
-    subscriber.on("message", function(channel, message){
-        console.log(typeof channel)
-        console.log(channel, message)
-        io.to(channel).emit('new-message', message)
-    })
-    
-    socket.on("send", function(data){
-        // send to redis
-        publisher.publish(`messages`, data)
-    });
-
-    socket.on("disconnect", function(){
-        subscriber.unsubscribe()
-        subscriber.quit()
-        publisher.quit()
-    })
-
-})
-
-io.listen(process.env.PORT)
+httpServer.listen(process.env.PORT, () =>
+  console.log(`server started on ${process.env.PORT}`)
+);
